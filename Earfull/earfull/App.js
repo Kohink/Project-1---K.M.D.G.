3bdff08bf706ff8cef962ccc14d389440b7992b2
gjrocky <gjb17@my.fsu.edu>
2022-09-27 20:47:36 -0400

    earfull authentification

---
+++ Earfull/earfull/App.js
@@ -0,0 +1,24 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { AuthProvider } from './hooks/useAuth';
+import StackNavigator from './StackNavigator';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <AuthProvider>
+        <StackNavigator/>
+      </AuthProvider>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
